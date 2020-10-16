aeb0f9909372bcb66bb23aa581c61f9b3f7efad5
marse00 <gmmarcelino8@gmail.com>
2020-10-16 16:11:04 +0100

    Created login component (React)
    *Sends a POST request when the login form is submitted

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+import LoginForm from "./components/Auth/login";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <LoginForm />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/Auth/login.jsx
@@ -0,0 +1,75 @@
+import React, { Component } from "react";
+import axios from "axios";
+
+let config = {
+  headers: {
+    withCredentials: true,
+    "Content-Type": "text/html",
+  },
+};
+
+class LoginForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: "",
+      password: "",
+    };
+  }
+  render() {
+    return (
+      <React.Fragment>
+        <h1>Login</h1>
+        <form onSubmit={this.handleSubmit}>
+          <input
+            type="username"
+            name="username"
+            value={this.state.username}
+            onChange={this.handleChange}
+            required
+          />
+          <input
+            type="password"
+            name="password"
+            value={this.state.password}
+            onChange={this.handleChange}
+            required
+          />
+          <button type="submit">Register</button>
+        </form>
+      </React.Fragment>
+    );
+  }
+  handleChange = (event) => {
+    console.log(event.target);
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  handleSubmit = () => {
+    const { username, password } = this.state;
+
+    axios
+      .post(
+        "http://localhost:8080/",
+
+        {
+          User: {
+            Username: username,
+            Password: password,
+          },
+        },
+
+        config
+      )
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
+}
+
+export default LoginForm;
